cf2fd1142f435c5a672d8fd34d2712d6bd6743fb
Gašper <gasperbr@proxima.si>
2021-05-18 20:18:53 +0200

    add comments for the valid order functions

--- src/orders/validOrders.ts
+++ src/orders/validOrders.ts
@@ -2,7 +2,9 @@ import { ILimitOrderData } from "limitorderv2-sdk";
 import { Database } from "../database/database";
 import { ILimitOrder } from "../models/models";
 
-// Checks if the order has already been filled or if we already send a tx for filling the order
+// This is called just before we try to execute orders
+// Check if the order is (still) valid
+// & update the order status in the database ** (move this to another function potentially)
 export async function validOrders(orders: ILimitOrder[]): Promise<ILimitOrder[]> {
 
   const database = Database.Instance;
@@ -12,20 +14,26 @@ export async function validOrders(orders: ILimitOrder[]): Promise<ILimitOrder[]>
   const validOrders = [];
 
   orders.forEach(order => {
-    // get order status
+    // TODO
+
+    // simulate the transaction
+
+    // get order status:
+    // check if the user has enough balance
     // filledOrders.push()
     // partiallyFilledOrders.push()
     // check that we have not already executed the order
     // validOrders.push()
-  })
+  });
 
-  await database.updateLimitOrders(filledOrders);
-  await database.deleteLimitOrders(partiallyFilledOrders);
+  await database.updateLimitOrders(filledOrders); // order has been partially filled
+  await database.deleteLimitOrders(partiallyFilledOrders); // order filled or expired
 
   return validOrders;
 }
 
-// validate amounts are > 0, chainId is correct etc..
+// This is called after we receive the order from the user
+// validate amounts are > 0, check the signature
 export function validLimitOrderData(order: ILimitOrderData): boolean {
   // TODO
   return true;
