3cf78fe86373b8d2d40926cbd606d3f085e5e342
Sarang Parikh <sarangparikh22@gmail.com>
2021-05-19 22:39:10 +0530

    Order Validation before Execution

--- src/orders/validOrders.ts
+++ src/orders/validOrders.ts
@@ -1,7 +1,8 @@
 import { ILimitOrderData, LimitOrder } from "limitorderv2-sdk";
 import { Database } from "../database/database";
 import { ILimitOrder } from "../models/models";
-import { utils } from 'ethers';
+import { utils, Contract, providers } from 'ethers';
+import stopLimitOrderABI from '../abis/stopLimitOrder';
 
 // This is called just before we try to execute orders
 // Check if the order is (still) valid
@@ -13,8 +14,18 @@ export async function validOrders(orders: ILimitOrder[]): Promise<ILimitOrder[]>
   const filledOrders = [];
   const validOrders = [];
 
-  orders.forEach(order => {
+  const provider = new providers.WebSocketProvider(process.env.MAINNET_WEBSOCKET_JSON_RPC);
+
+  // Add this to your env later
+  let stopLimitOrderContract = new Contract("0xce9365dB1C99897f04B3923C03ba9a5f80E8DB87", stopLimitOrderABI, provider);
+
+  orders.forEach(async (order) => {
     // TODO
+    let limitOrder = LimitOrder.getLimitOrder(order.order)
+    let isValidOrder = await (isOrderFilled(limitOrder, stopLimitOrderContract) || isOrderCancel(limitOrder, stopLimitOrderContract) || checkExpiry(limitOrder));
+    if(isValidOrder) validOrders.push(order);
+    else filledOrders.push(order);
+
     // get order status...
     // check if the user has enough balance
     // check if the order isn't expired
@@ -26,6 +37,21 @@ export async function validOrders(orders: ILimitOrder[]): Promise<ILimitOrder[]>
 
   return validOrders;
 }
+
+async function isOrderFilled(limitOrder: LimitOrder, stopLimitOrderContract: Contract): bool {
+  let digest = limitOrder.getTypeHash(limitOrder.chainId);
+  let orderStatus = await stopLimitOrderContract.cancelledOrder(limitOrder.maker, digest);
+  if(orderStatus) return false;
+  return true;
+}
+
+async function isOrderCancel(limitOrder: LimitOrder, stopLimitOrderContract: Contract): bool {
+  let digest = limitOrder.getTypeHash(limitOrder.chainId);
+  let orderStatus = await stopLimitOrderContract.orderStatus(digest);
+  if(Number(orderStatus) == Number(limitOrder.amountOutRaw)) return false;
+  return true;
+}
+
 
 // This is called after we receive the order from the user
 /*
