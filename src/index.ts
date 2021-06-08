b22c73035129cf83aa2736d4591fe6f3c66c3b60
Gašper <gasperbr@proxima.si>
2021-06-08 14:36:09 +0200

    add env validation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { watchSushiwapPairs } from './price-updates/pair-updates';
 import { watchLimitOrders, stopReceivingOrders } from './orders/txReceiver';
 import { executeOrders } from './orders/execute';
 import { LimitOrderRelayer } from './LimitOrderRelayer';
+import dotenv from 'dotenv';
+import { validateEnv } from './utils/validateEnv';
+
+dotenv.config();
+
+validateEnv();
 
 new LimitOrderRelayer(watchLimitOrders, watchSushiwapPairs, Database.Instance, executeOrders).init();
 
