6e97f2be55a87f92297198084e5ba3c3f1ece26c
unknown <gasper.brvar@gmail.com>
2021-06-11 16:30:47 +0200

    setup forever

---
+++ src/database-scripts/copy-remote.ts
@@ -0,0 +1,22 @@
+/* script used to seed the relayers database with orders */
+
+import axios from 'axios';
+import { ILimitOrderData, LAMBDA_URL } from 'limitorderv2-sdk';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export async function copyRemoteOrders() {
+
+  const orders: ILimitOrderData[] = (await axios(`${LAMBDA_URL}/orders/pending`, {
+    method: 'POST',
+    data: {
+      chainId: process.env.CHAINID
+    }
+  })).data.data;
+
+  console.log(orders.);
+
+
+}
+
+copyRemoteOrders();
