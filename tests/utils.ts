f74a0ef6ad464183ba4bb2613fa40b76689e8172
unknown <gasper.brvar@gmail.com>
2021-06-18 17:47:38 +0200

    add test for gas price fetching

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,6 +1,8 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { ChainId } from '@sushiswap/sdk';
 import { expect } from 'chai';
 import { PRICE_MULTIPLIER } from '../src/price-updates/pair-updates';
+import { getGweiGasPrice } from '../src/utils/network';
 import { getOrderPrice } from '../src/utils/price';
 
 describe('Utils', () => {
@@ -11,4 +13,13 @@ describe('Utils', () => {
     expect(price.toString()).to.be.eq("300902708124373119358");
     expect(BigNumber.from("300902708124373119358").mul("997").div("1000").add(1).toString()).to.be.eq("300000000000000000000")
   });
+
+  it.only('Should get gas price', async () => {
+    const supportedChains = [ChainId.MAINNET, ChainId.MATIC];
+    const prices = await Promise.all(supportedChains.map(chain => {
+      return getGweiGasPrice(chain);
+    }));
+    const errIndex = prices.indexOf(undefined);
+    expect(errIndex).to.be.equal(-1, `Couldn't fetch gas price for ${supportedChains[errIndex]}`)
+  }).timeout(4000);
 });
