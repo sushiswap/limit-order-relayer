512faebe9ae58f03a307374e8a9a0284d8e2a227
Sarang Parikh <sarangparikh22@gmail.com>
2022-01-25 04:16:12 +0530

    ethereum deployment

--- src/relayer-config/pairs.ts
+++ src/relayer-config/pairs.ts
@@ -21,7 +21,50 @@ export const fetchLimitOrderPairs = function (chainId: ChainId): string[][] {
 }
 
 export const _limitOrderPairs = {
-  [ChainId.ETHEREUM]: [['WETH', 'SUSHI']],
+  [ChainId.ETHEREUM]: [
+    ['WETH', 'SUSHI'],
+    ['ILV', 'WETH'],
+    ['USDC', 'WETH'],
+    ['WETH', 'USDT'],
+    ['TOKE', 'WETH'],
+    ['WBTC', 'WETH'],
+    ['BIT', 'WETH'],
+    ['WETH', 'ALCX'],
+    ['DAI', 'WETH'],
+    ['SUSHI', 'WETH'],
+    ['SPELL', 'WETH'],
+    ['DAI', 'wstETH'],
+    ['KP3R', 'WETH'],
+    ['AAVE', 'WETH'],
+    ['SYN', 'WETH'],
+    ['stkATOM', 'WETH'],
+    ['WETH', 'RGT'],
+    ['YFI', 'WETH'],
+    ['SUSHI', 'FRAX'],
+    ['FODL', 'USDC'],
+    ['WBTC', 'BADGER'],
+    ['FTM', 'WETH'],
+    ['MKR', 'WETH'],
+    ['FODL', 'WETH'],
+    ['BENT', 'DAI'],
+    ['DELTA', 'WETH'],
+    ['xSUSHI', 'WETH'],
+    ['NEWO', 'USDC'],
+    ['WXRP', 'WETH'],
+    ['WETH', 'ANY'],
+    ['YGG', 'WETH'],
+    ['LDO', 'WETH'],
+    ['CVX', 'WETH'],
+    ['SRM', 'WETH'],
+    ['LINK', 'WETH'],
+    ['LON', 'USDT'],
+    ['BSGG', 'WETH'],
+    ['COMP', 'WETH'],
+    ['NTFX', 'WETH'],
+    ['PERP', 'WETH'],
+    ['WETH', 'ICE'],
+    ['PUNK', 'WETH'],
+  ],
   [ChainId.MATIC]: [
     ['WETH', 'WMATIC'],
     ['WETH', 'USDC'],
@@ -98,5 +141,7 @@ export const getDesiredProfitToken = function (chainId: ChainId): string[] {
     return ['WMATIC', 'WETH', 'SUSHI', 'WBTC', 'USDC', 'DAI', 'USDT']
   } else if (chainId == ChainId.AVALANCHE) {
     return ['WAVAX', 'TIME', 'MIM']
+  } else if (chainId == ChainId.ETHEREUM) {
+    return ['WETH', 'MIM', 'DAI', 'USDC', 'WBTC', 'USDT']
   }
 }
